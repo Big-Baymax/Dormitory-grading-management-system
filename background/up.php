710e20ad896add4af2ff3905cfc7d38f8667abee
daibai <764929841@qq.com>
2017-06-17 18:32:31 +0800

    开始，初步完成大部分功能

--- background/up.php
+++ background/up.php
@@ -11,6 +11,7 @@ if($database->update('message', ['up[+]' => 1], ['id' => $_GET['id']])){
     $up = $database->select('message', 'up', ['id' => $_GET['id']]);
     echo $up[0];
 }
+
 else{
     echo 'erro';
 }
